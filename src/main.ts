a25cac0933702e0c7305dca0eddfc24714cac34c
dippp <Dipppp84@gmail.com>
2022-07-17 15:03:08 +0300

    feat: add .env

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
+import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
+console.log('ALOOO');
+console.log(process.env.PORT);
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(4000);
+  await app.listen(process.env.PORT || 4000);
 }
 
 bootstrap();
